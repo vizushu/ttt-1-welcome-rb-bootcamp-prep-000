2445872da300a484293100d8285f8f3b4a30cbea
Renier De La Cruz <me@renierdelacruz.com>
2019-05-31 18:37:17 +0000

    Automatically backed up by Learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,5 +1,5 @@
-def prints
-  puts "Welcome to Tic Tac Toe!"
+def prints("Welcome to Tic Tac Toe!")
+  print
 end
 # You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
